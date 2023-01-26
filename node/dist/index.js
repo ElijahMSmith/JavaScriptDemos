0199e65fe64c67a9a87d0acbd3bc6a992b78cfd6
ElijahMSmith <mcditto99@gmail.com>
2023-01-26 17:28:09 -0500

    node demo

---
+++ node/dist/index.js
@@ -0,0 +1,6 @@
+console.log("test");
+var x = 3;
+var testFn = function (arg1, arg2) {
+    return arg1 + arg2;
+};
+console.log(testFn(4, "function"));
---
+++ node/index.ts
@@ -0,0 +1,9 @@
+console.log("test");
+
+let x = 3;
+
+const testFn = (arg1: number, arg2: string): string => {
+	return arg1 + arg2;
+};
+
+console.log(testFn(4, "function"));
